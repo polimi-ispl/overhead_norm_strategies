65d4d7e7c47b61a8f47bdadd1e8e6121fb622157
Edoardo Daniele Cannas <edoardodaniele.cannas@polimi.it>
2023-12-05 13:10:23 +0100

    parallel test script

--- bash_scripts/test_all_parallel.sh
+++ bash_scripts/test_all_parallel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # USER PARAMETERS (put your device configuration params here)
-DEVICE=0
+DEVICE=0  # PUT THERE YOUR GPU ID
 SGD_DIR=../data/spliced_images/sgd # path to SGD dataset (YOUR PATH MAY BE DIFFERENT IF YOU SAVED THE SPLITS IN ANOTHER FOLDER, CHECK IT)
 HEGD_DIR=../data/spliced_images/hegd # path to HEGD dataset (YOUR PATH MAY BE DIFFERENT IF YOU SAVED THE SPLITS IN ANOTHER FOLDER, CHECK IT)
 RESULTS_DIR=../results # path to save the results (YOUR PATH MAY BE DIFFERENT IF YOU SAVED THE SPLITS IN ANOTHER FOLDER, CHECK IT)
@@ -13,13 +13,13 @@ echo "| Testing all model in parallel |"
 echo "-------------------------------------------------"
 
 (trap 'kill 0' SIGINT;
-python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MinPMax_99/model_weights.h5 \
+python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MinPMax_99_mean-scaling_False/model_weights.h5 \
 --root_dir $SGD_DIR --results_dir $RESULTS_DIR --preprocessing_dir $PREPROCESSING_DIR & \
-python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MinPMax_99/model_weights.h5 \
+python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MinPMax_99_mean-scaling_False/model_weights.h5 \
 --root_dir $HEGD_DIR --results_dir $RESULTS_DIR --preprocessing_dir $PREPROCESSING_DIR & \
-python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MinPMax_95/model_weights.h5 \
+python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MinPMax_95_mean-scaling_False/model_weights.h5 \
 --root_dir $SGD_DIR --results_dir $RESULTS_DIR --preprocessing_dir $PREPROCESSING_DIR & \
-python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MinPMax_95/model_weights.h5 \
+python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MinPMax_95_mean-scaling_False/model_weights.h5 \
 --root_dir $HEGD_DIR --results_dir $RESULTS_DIR --preprocessing_dir $PREPROCESSING_DIR & \
 python ../test_with_AUCs.py --gpu $DEVICE --fe_path ../models/MaxScaling/model_weights.h5 \
 --root_dir $SGD_DIR --results_dir $RESULTS_DIR & \
